a6f3074feb8cf2dbfebdfa6cc0bdde6a2f762e9c
nyanmy <nyanmyth100@gmail.com>
2021-04-16 15:34:34 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name phone_number birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name phone_number birthday])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'topics/index'
   root to: "topics#index"
+  resources :users, only: [:edit, :update]
 end
