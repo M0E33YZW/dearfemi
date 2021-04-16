395b75354937bf7a4079b9e8de81b0a58e541e49
nyanmy <nyanmyth100@gmail.com>
2021-04-16 15:52:27 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :birthday
+  end
+
+  has_many :topics
 end
