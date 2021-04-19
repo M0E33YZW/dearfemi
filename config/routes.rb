c6ebc066c9a422d7ad2ff9b3ba39eb9f408eac94
nyanmy <nyanmyth100@gmail.com>
2021-04-19 17:50:20 +0900

    詳細ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "topics#index"
   resources :users, only: [:edit, :update]
-  resources :topics, only: [:index, :new, :create] do
+  resources :topics, only: [:index, :new, :create, :show] do
     collection do
       get 'about'
     end
