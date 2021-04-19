f48caed88752ee7ed35e07a28d661019c45dbb45
nyanmy <nyanmyth100@gmail.com>
2021-04-19 15:42:01 +0900

    トピック投稿機能の実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,25 @@
 class TopicsController < ApplicationController
   def index
+    query = "SELECT * FROM topics"
+    @topics = Topic.find_by_sql(query)
+  end
+
+  def new
+    @topic = Topic.new
+  end
+  
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to topics_path
+    else
+      render :index
+    end    
+  end
+
+  private
+  
+  def topic_params
+    params.require(:topic).permit(:title, :text, :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'topics/index'
   root to: "topics#index"
   resources :users, only: [:edit, :update]
+  resources :topics, only: [:index, :new, :create] 
 end
