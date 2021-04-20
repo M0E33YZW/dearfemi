f6478e2316905f4acb20b75951468e672987fb4e
nyanmy <nyanmyth100@gmail.com>
2021-04-20 13:55:24 +0900

    コメント削除機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,11 @@ class CommentsController < ApplicationController
     redirect_to "/topics/#{comment.topic.id}"
   end
 
+  def destroy
+    Comment.find_by(id: params[:id],topic_id: params[:topic_id]).destroy
+    redirect_to topics_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, topic_id: params[:topic_id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'topics#index'
   resources :users, only: %i[edit update show]
   resources :topics do
-    resources :comments, only: :create
+    resources :comments, only: %i[create destroy]
     collection do
       get 'about'
     end
