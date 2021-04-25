4d7f03e8c169c28895534ae4ae358fd5f103a2ca
nyanmy <nyanmyth100@gmail.com>
2021-04-25 10:16:39 +0900

    モデルを編集

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  has_many :topic_tag_relations
+  has_many :topic_tag_relations, dependent: :destroy
   has_many :topics, through: :topic_tag_relations
 
-  validates :tagname, uniqueness: true
+  validates :tagname, uniqueness: true, length: { maximum: 30 }
 end
--- app/models/topic_tag_relation.rb
+++ app/models/topic_tag_relation.rb
@@ -1,4 +1,9 @@
 class TopicTagRelation < ApplicationRecord
   belongs_to :topic
   belongs_to :tag
+
+  with_options presence: true do
+    validates :topic_id
+    validates :tag_id
+  end  
 end
