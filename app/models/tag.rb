7ae264f9027f13da9613648b65064ca9334ee97c
nyanmy <nyanmyth100@gmail.com>
2021-06-07 21:20:26 +0900

    Topicモデル単体テストコード

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :topic_tag_relations, dependent: :destroy
   has_many :topics, through: :topic_tag_relations
 
-  validates :tagname, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :tagname, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
 end
--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :topic do
+    title {Faker::Name.name}
+    text  {Faker::Lorem.sentence}
+    association :user
+    
+    after(:build) do |topic|
+      topic.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
